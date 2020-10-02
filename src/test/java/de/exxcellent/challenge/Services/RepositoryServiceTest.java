f44c9828c6417803e891409836aed5edfb30ead3
Daniel Knizia <daakni@gmail.com>
2020-10-02 09:54:09 +0200

    Added test for reading csv with RepositoryService

---
+++ src/test/java/de/exxcellent/challenge/Services/RepositoryServiceTest.java
@@ -0,0 +1,38 @@
+package de.exxcellent.challenge.Services;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RepositoryServiceTest {
+
+    @BeforeAll
+    public static void setupCSV() {
+        try (FileWriter fileWriter = new FileWriter("test.csv");){
+            String data = "A,B,C\nB,1,2\nC,2,3" ;
+            fileWriter.write(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testReadCSVFile(){
+        RepositoryService repositoryService = new RepositoryService();
+        FileWrapper rawData = null;
+        try {
+            rawData = repositoryService.get("test.csv");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        List<String> testData = new ArrayList<>(Arrays.asList("A,B,C", "B,1,2","C,2,3"));
+        assertEquals(testData, rawData.getFileData());
+    }
+}
